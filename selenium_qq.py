94eea5ac99acf73c03f350ea63755a4b3aea3b13
414aj <2675849542@qq.com>
2018-04-21 17:38:11 +0800

    add comment

--- selenium_qq.py
+++ selenium_qq.py
@@ -1,4 +1,7 @@
 #!python3
+#使用selenium模拟登陆qq邮箱并发送邮件
+#目前能正常登陆邮箱
+#写邮件时，邮件内容无法发送到正文内容区域，而是发送到了收件人输入框里
 
 import sys,time
 from selenium import webdriver
@@ -6,32 +9,33 @@ from selenium.webdriver.common.keys import Keys
 
 browser=webdriver.Firefox()
 browser.get("https://mail.qq.com")
-time.sleep(5)
+time.sleep(5) #等待页面加载完成
 
-browser.switch_to.frame("login_frame")
-browser.find_element_by_xpath("/html/body/div/div/div/a[2]").click()
-uElem=browser.find_element_by_name('u')
+browser.switch_to.frame("login_frame") #跳转到login_frame
+browser.find_element_by_xpath("/html/body/div/div/div/a[2]").click() #使用账户和密码登陆
+uElem=browser.find_element_by_name('u') #依次发送账号和密码，并点击登陆
 uElem.clear()
-uElem.send_keys('2675849542')
+uElem.send_keys('xxxxxxxxxxxxx')
 pElem=browser.find_element_by_name('p')
 pElem.clear()
-pElem.send_keys('201314asdb')
+pElem.send_keys('xxxxxxxxxxxxx')
 browser.find_element_by_id("login_button").click()
-time.sleep(5)
+time.sleep(5) #等待页面加载完成
 
-browser.switch_to.default_content()
-browser.find_element_by_link_text('写信').click()
-time.sleep(5)
-browser.switch_to.frame('mainFrame')
-aElem=browser.find_element_by_xpath("//*[@id='toAreaCtrl']/div[2]/input")
-aElem.send_keys('aj_is_me@qq.com')
+browser.switch_to.default_content() #跳转到主界面
+browser.find_element_by_link_text('写信').click() #点击写信按键
+time.sleep(5) #等待页面加载完成
 
-frame=browser.find_element_by_xpath("//*[@id='QMEditorArea']/table/tbody/tr/td/iframe")
-browser.switch_to.frame(frame)
-cElem=browser.find_element_by_xpath('/html/body')
-cElem.send_keys("xxxxxxxxx")
+browser.switch_to.frame('mainFrame') #跳转到mainFrame
+aElem=browser.find_element_by_xpath("//*[@id='toAreaCtrl']/div[2]/input") #定位收件人
+aElem.send_keys('xxxxxxxxxxxxx') #发送收件人邮箱
+     
+frame=browser.find_element_by_xpath("//*[@id='QMEditorArea']/table/tbody/tr/td/iframe") #定位邮件正文的frame
+browser.switch_to.frame(frame) #跳转到frame
+cElem=browser.find_element_by_xpath('/html/body') #定位邮件正文内容区域
+cElem.send_keys("xxxxxxxxx") #发送邮件正文
 
-browser.switch_to.parent_frame()
-browser.find_element_by_link_text('发送').click()
-time.sleep(5)
-browser.close()
+browser.switch_to.parent_frame() #回到mainFrame
+browser.find_element_by_link_text('发送').click() #点击发送邮件
+time.sleep(5) #等待页面加载
+browser.close() #关闭browser
